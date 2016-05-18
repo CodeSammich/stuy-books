dd7d3bde3ad5b31e7ac85799079df43ccb403d4c
JeffreyZou13 <jeffreyzou13@gmail.com>
2016-05-17 21:13:32 -0400

    some database stuff

--- accounts.py~
+++
@@ -1,6 +0,0 @@
-def check_valid_email( email ):
-    if len(email) >= 10:
-        if email[-9:] == "@stuy.edu":
-            return True
-    return False
-
---
+++ database.py
@@ -0,0 +1,72 @@
+# Authors: Jeffrey Zou
+# Description: Deals with the database
+
+from pymongo import MongoClient
+
+connection = MongoClient()
+
+def replaceApostrophe(s):
+    return s.replace("'", '&#8217')
+
+#------------------------- Setup User -------------------------#
+def addUser(email, passwordHash, first, last):
+    '''
+    Adds user to the database
+    Args:
+        email (string)
+        passwordHash (string)
+        first (string): first name of the user
+        last (string): last name of the user
+    Returns:
+        String with errors, or empty string if there aren't any
+    '''
+    db = connection['Users']
+    if email.find('@stuy.edu') == -1:
+        return 'You must register with your stuy.edu account'
+    useraccounts = db.accounts.find({'email':email})
+    if len(r) != 0:
+        return 'An account has already been registered under this email'
+    db.accounts.insert_one({
+        'email': replaceApostrophe(email),
+        'passwordHash': passwordHash,
+        'first': first,
+        'last': last
+    })
+    return ''
+
+def authenticate(email, passwordHash):
+    '''
+    Checks to see if email and password exist in the database
+    args:
+        email (string)
+        passwordHash (string)
+    Returns:
+        True if the password and email exist in the database, False otherwise
+    '''
+    db = connection['Users']
+    result = db.accounts.find_one({'email': email})
+    if len(result) == 0:
+        return False
+    return result['passwordHash'] == passwordHash
+
+def updatePassword(email, newPasswordHash):
+    '''
+    Updates the password for the user
+    Args:
+        email (string)
+        newPasswordHash (string)
+    Returns:
+        True if successful, False otherwise
+    '''
+    db = connection['Users']
+    result = db.accounts.find_one({'email': email})
+    if len(result) == 0:
+        return False
+    else:
+        db.accounts.update_one(
+            {'email':email},
+            {
+                '$set': {'passwordHash': newPasswordHash}
+            }
+        )
+        return True
