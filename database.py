a8f5461269105dec6b06a4d1f24a0ad0dacdf1d7
Samuel Zhang <samuelzhang3@gmail.com>
2016-05-22 01:10:08 -0400

    database structure in addUser updated, frozen on servertimeout

--- database.py
+++ database.py
@@ -1,9 +1,14 @@
-# Authors: Jeffrey Zou
+# Authors: Samuel Zhang, Jeffrey Zou
 # Description: Deals with the database
 
 from pymongo import MongoClient
 
-connection = MongoClient()
+#------------------------- Establish MongoDB Connection ----------------#
+client = MongoClient()
+
+#### Data is stored in databases, under which are collections.
+#### Ex. "accounts-database" is a database, and "accounts" is the collection
+#### "accounts" contains all the "Documents" that have each user's information
 
 def replaceApostrophe(s):
     return s.replace("'", '&#8217')
@@ -15,17 +20,34 @@ def addUser(email, passwordHash):
     Args:
         email (string)
         passwordHash (string)
-        first (string): first name of the user
-        last (string): last name of the user
     Returns:
         String with errors, or empty string if there aren't any
     '''
-    db = connection['Users']
-    useraccounts = db.accounts.find_one({'email': email})
-    if useraccounts != None:
+    print 'starting'
+    dbnames = client.database_names()
+    print 'next'
+    db = client['accounts-database']
+    print 'database called'
+    accounts = db['accounts']
+    print 'before empty db test'
+    if 'accounts-database' not in dbnames: #init database and collection
+        print 'database initialized'
+        #generating password hex for init account
+        m = sha256()
+        m.update( "dummy_password" ) #may need to be more secure
+        dummy_pass = m.hexdigest()
+        init_account = {
+            'email': 'dummy_email@stuy.edu',
+            'passwordHash': dummy_pass,
+        }
+        init_id = accounts.insert_one( init_account).inserted_id #dummy account
+    print 'after empty test'
+    user_account = accounts.find_one({'email': email})
+    print "looked through db"
+    if user_account != None:
         print 'no go'
         return 'An account has already been registered under this email'
-    db.accounts.insert_one({
+    accounts.insert_one({
         'email': replaceApostrophe(email),
         'passwordHash': passwordHash
         #'first': first,
@@ -42,8 +64,8 @@ def authenticate(email, passwordHash):
     Returns:
         True if the password and email exist in the database, False otherwise
     '''
-    db = connection['Users']
-    result = db.accounts.find_one({'email': email}) #definitely only 1 acc.
+    db = client['accounts-database']
+    result = accounts.find_one({'email': email}) #definitely only 1 acc.
     if result == None:
         return False
     return True
@@ -61,7 +83,7 @@ def updatePassword(email, newPasswordHash):
     if len(result) == 0:
         return False
     else:'''
-    db = connection['Users']
+    db = client['accounts-database']
     try:
         db.accounts.update_one(
             {'email':email},
