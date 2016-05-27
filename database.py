0d7bfb0b22dd208847572f6d9615c0cfc019f6fb
Samuel Zhang <samuelzhang3@gmail.com>
2016-05-27 01:27:59 -0400

    search fixed

--- database.py
+++ database.py
@@ -300,21 +300,24 @@ def searchForBook(query):
         A list of books with that name/author
     '''
     db = client['books-database']
-    books = db['books']
-    print books.count()
+    books = db['books'] #collection
 
     #parse query to find relevant results
     results = []
     query = query.strip(',')
     query = query.split(' ')
-    print query[0]
-    print books[1]['bookName']
     print query
-    for i in range(books.count()): #goes through book database
-        for j in range(len(query)): #goes through query
-            if books[i]['bookName'].find(query[j]) != -1: #if found
-                print 'book found, going to next book'
-                results.append( books[i] )
-                break; #goes to next book
+    for j in range(len(query)): #goes through query
 
-    return results
+        # goes through database to find books with query[j] in substring value
+        cursor = books.find( {'bookName':
+                              { '$regex' : '.*' + query[j] + '.*' } } )
+        for b in cursor:
+            print b['bookName']
+            print 'book found, going to next book'
+            results.append( b )
+            break; #goes to next book
+        
+        return results
+
+searchForBook( "Calc" )
